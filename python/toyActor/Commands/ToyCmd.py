d04b4059d9d05ee5ca393f1ec0922d6df8d4d9f8
Craig Loomis <cloomis@astro.princeton.edu>
2012-10-26 11:22:31 -0400

    lean things down

--- python/toyActor/Commands/ToyCmd.py
+++ python/toyActor/Commands/ToyCmd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import logging
+import time
 
 import opscore.protocols.keys as keys
 import opscore.protocols.types as types
@@ -9,75 +9,75 @@ from opscore.utility.qstr import qstr
 
 class ToyCmd(object):
 
-	def __init__(self, actor):
-        # This is essential:
-		self.actor = actor
+    def __init__(self, actor):
+        # This lets us access the rest of the actor.
+        self.actor = actor
 
-		# Define some typed command arguemetnts
-		self.keys = keys.KeysDictionary("toy_toy", (1, 1),
-										keys.Key("cartridge", types.Int(), help="A cartridge ID"),
-										keys.Key("actor", types.String(), help="Another actor to command"),
-										keys.Key("cmd", types.String(), help="A command string"),
-										keys.Key("cnt", types.Int(), help="A count of things to do"))
-		#
-		# Declare commands
-		#
-		self.vocab = [
-			('ping', '', self.ping),
-			('status', '', self.status),
-			('doSomething', '<cnt>', self.doSomething),
-			('passAlong', '<actor> <cmd>', self.passAlong),
-		]
+        # Declare the commands we implement. When the actor is started
+        # these are registered with the parser, which will call the
+        # associated methods when matched. The callbacks will be
+        # passed a single argument, the parsed and typed command.
+        #
+        self.vocab = [
+            ('ping', '', self.ping),
+            ('status', '', self.status),
+            ('doSomething', '<cnt> [<delay>] [<keyCnt>] [<keyLen>]', self.doSomething),
+            ('passAlong', '<actor> <cmd>', self.passAlong),
+        ]
 
-	def ping(self, cmd):
-		'''Query the actor for liveness/happiness.'''
+        # Define typed command arguments for the above commands.
+        self.keys = keys.KeysDictionary("toy_toy", (1, 1),
+                                        keys.Key("actor", types.String(), help="Another actor to command"),
+                                        keys.Key("cmd", types.String(), help="A command string"),
+                                        keys.Key("keyCnt", types.Int(), help="A count of keys to return"),
+                                        keys.Key("keyLen", types.Int(), help="The length of the keys to return"),
+                                        keys.Key("cnt", types.Int(), help="A count of things to do"),
+                                        keys.Key("delay", types.Float(), help="Seconds to delay"))
+        #
+    def ping(self, cmd):
+        """Query the actor for liveness/happiness."""
 
-		cmd.finish("text='Present and (probably) well'")
+        # 
+        cmd.finish("text='Present and (probably) well'")
 
-	def status(self, cmd):
-		'''Report status and version; obtain and send current data'''
+    def status(self, cmd):
+        """Report status and version; obtain and send current data"""
 
-		self.actor.sendVersionKey(cmd)
-		self.doStatus(cmd, flushCache=True)
+        self.actor.sendVersionKey(cmd)
 
-	def doStatus(self, cmd=None, flushCache=False, doFinish=True):
-		'''Report full status'''
+        keyStrings = ['text="nothing to say, really"']
+        keyMsg = '; '.join(keyStrings)
 
-		if not cmd:
-			cmd = self.actor.bcast
+        cmd.inform(keyMsg)
+        cmd.diag('text="still nothing to say"')
+        cmd.finish()
 
-		keyStrings = ['text="nothing to say, really"']
-		keyMsg = '; '.join(keyStrings)
+    def doSomething(self, cmd):
+        """ Do something pointless. """
 
-		cmd.inform(keyMsg)
-		cmd.diag('text="still nothing to say"')
-		cmd.finish()
+        cnt = cmd.cmd.keywords["cnt"].values[0]
+        delay = cmd.cmd.keywords["delay"].values[0] if "delay" in cmd.cmd.keywords else 0.0
+        keyCnt = cmd.cmd.keywords["keyCnt"].values[0] if "keyCnt" in cmd.cmd.keywords else 1
+        keyLen = cmd.cmd.keywords["keyLen"].values[0] if "keyLen" in cmd.cmd.keywords else 1
+        
+        for i in range(cnt):
+            keyVals = ["%0.2f" % (kl_i) for kl_i in range(keyLen)]
+            keys = ["key%04d=%s" % (k_i, ",".join(keyVals)) for k_i in range(keyCnt)]
+            cmd.inform('cnt=%d; %s' % (i, ";".join(keys)))
+            if delay:
+                time.sleep(delay)
+        cmd.finish()
 
-	def doSomething(self, cmd):
-		""" Do something pointless. """
+    def passAlong(self, cmd):
+        """ Pass a command along to another actor. """
 
-		cnt = cmd.cmd.keywords["cnt"].values[0]
-		for i in range(cnt):
-			cmd.inform('cnt=%d' % (i))
-		cmd.finish()
+        actor = cmd.cmd.keywords["actor"].values[0]
+        cmdString = cmd.cmd.keywords["cmd"].values[0]
 
-	def passAlong(self, cmd):
-		""" Pass a command along to another actor. """
-
-		actor = cmd.cmd.keywords["actor"].values[0]
-		cmdString = cmd.cmd.keywords["cmd"].values[0]
-
-		cmdVar = self.actor.cmdr.call(actor=actor, cmdStr=cmdString,
+        cmdVar = self.actor.cmdr.call(actor=actor, cmdStr=cmdString,
                                       forUserCmd=cmd, timeLim=30.0)
-		if cmdVar.didFail:
-			cmd.fail('text=%s' % (qstr('Failed to pass %s along to %s' % (cmdStr, actor))))
-		else:
-			cmd.finish()
-
-	def sendingCommands(self, cmd):
-		""" Examples of sending commands to other actors. """
-		
-		cmdVar = self.actor.cmdr.call(actor='tcc', cmdStr='axis status', timeLim=3.0)
-		if cmdVar.didFail:
-			cmd.warn('text=\'Failed to fetch TCC axis status\'')
+        if cmdVar.didFail:
+            cmd.fail('text=%s' % (qstr('Failed to pass %s along to %s' % (cmdStr, actor))))
+        else:
+            cmd.finish()
 
--- python/toyActor/toyActor_main.py
+++ python/toyActor/toyActor_main.py
@@ -1,53 +1,27 @@
 #!/usr/bin/env python
 
-from twisted.internet import reactor
+import opscore.utility.sdss3logging
 
 import opscore.actor.model
-import opscore.actor.keyvar
-
 import actorcore.Actor
-import actorkeys
-
-# Import sdss3logging before logging if you want to use it
-#
-import opscore.utility.sdss3logging as sdss3logging
-import logging
 
 class Toy(actorcore.Actor.Actor):
     def __init__(self, name, productName=None, configFile=None, debugLevel=30):
-        actorcore.Actor.Actor.__init__(self, name, productName=productName, configFile=configFile)
-
-        self.headURL = '$HeadURL: svn+ssh://sdss3svn@sdss3.org/repo/ops/actors/apoActor/trunk/python/apoActor/apoActor_main.py $'
-
-        self.logger.setLevel(debugLevel)
-        self.logger.propagate = True
-
+        # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
-        # Explicitly load other actor models. We usually need these for FITS headers.
+        actorcore.Actor.Actor.__init__(self, name, productName=productName, configFile=configFile)
+        #self.logger.setLevel(debugLevel)
+        
+        # Explicitly declare which actors we need to know about, so we can access their keywords.
         #
         self.models = {}
-        for actor in ["mcp", "guider", "platedb", "tcc"]:
+        for actor in []:
             self.models[actor] = opscore.actor.model.Model(actor)
-
-        # Finish by starting the twisted reactor
-        #
-        self.run()
-
-    def periodicStatus(self):
-        '''Run some command periodically'''
-
-        self.callCommand('status')
-        reactor.callLater(int(self.config.get(self.name, 'updateInterval')), self.periodicStatus)
-
-    def connectionMade(self):
-        '''Runs this after a connection is made from the hub'''
-
-        # Schedule an update.
-        #
-        reactor.callLater(3, self.periodicStatus)
 
 #
 # To work
-#
+
 if __name__ == '__main__':
     toy = Toy('toy', 'toyActor')
+    toy.run()
+    
