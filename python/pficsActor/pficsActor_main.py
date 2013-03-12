17962d91121a295bbc15825f27b4b0408e5c8666
Craig Loomis <cloomis@astro.princeton.edu>
2013-03-12 01:51:34 -0400

    push .model{} management off to Actor

--- python/pficsActor/pficsActor_main.py
+++ python/pficsActor/pficsActor_main.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python
 
-import opscore.utility.sdss3logging
-
-import opscore.actor.model
 import actorcore.Actor
 
 class Pfics(actorcore.Actor.Actor):
     def __init__(self, name, productName=None, configFile=None, debugLevel=30):
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
-        actorcore.Actor.Actor.__init__(self, name, productName=productName, configFile=configFile)
+        actorcore.Actor.Actor.__init__(self, name, productName=productName, configFile=configFile,
+                                       modelNames=('mcs','mps'))
         
-        # Explicitly declare which actors we need to know about, so we can access their keywords.
-        #
-        self.models = {}
-        for actor in ['mcs']:
-            self.models[actor] = opscore.actor.model.Model(actor)
-
-#
-# To work
-
-if __name__ == '__main__':
+def main():
     pfics = Pfics('pfics', 'pficsActor')
     pfics.run()
+
+if __name__ == '__main__':
+    main()
     
