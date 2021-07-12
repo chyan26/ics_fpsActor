eeacaa9a04c010bb6af4d138d02bf451618d8cd6
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2021-07-12 16:09:49 +0800

    Fixing bug after merging

--- python/ics/fpsActor/najaVenator.py
+++ python/ics/fpsActor/najaVenator.py
@@ -23,9 +23,17 @@ class NajaVenator(object):
     def conn(self):
         if self._conn is not None:
             return self._conn
+        pwpath=os.path.join(os.environ['ICS_FPSACTOR_DIR'],
+                            "etc", "dbpasswd.cfg")
 
         try:
-            connString = "dbname='opdb' user='pfs' host="+self.db
+            file = open(pwpath, "r")
+            passstring = file.read()
+        except:
+            raise RuntimeError(f"could not get db password from {pwpath}")
+
+        try:
+            connString = "dbname='opdb_asrd' user='pfs' host="+self.db+" password="+passstring
             # Skipself.actor.logger.info(f'connecting to {connString}')
             conn = psycopg2.connect(connString)
             self._conn = conn
@@ -83,14 +91,14 @@ class NajaVenator(object):
 
         return df
 
-    def readCentroid(self, frameId, moveId):
+    def readCentroid(self, frameId):
         """ Read centroid information from databse"""
         conn = self.conn 
 
         buf = io.StringIO()
 
-        cmd = f"""copy (select "mcsId", "fiberId", "centroidx", "centroidy" from "mcsData"
-                where "frameId"={frameId} and "moveId"={moveId}) to stdout delimiter ',' """
+        cmd = f"""copy (select "mcs_frame_id", "spot_id", "mcs_center_x_pix", "mcs_center_y_pix" from "mcs_data"
+                where "mcs_frame_id"={frameId}) to stdout delimiter ',' """
 
         with conn:
             with conn.cursor() as curs:
