5170bc09e83e265538cb91b1b60433e7c10cf90a
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2022-05-18 20:34:44 -1000

    Using makeMotorMap in dot crossing script.

--- bin/phiCross.sh
+++ bin/phiCross.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-steps=-30
+steps=-40
 count=40
 exptime=0.8
 
@@ -10,7 +10,9 @@ oneCmd.py iic moveToPfsDesign designId=0x4993e56d9549f2f9
 oneCmd.py mcs expose object exptime=$exptime doFibreID
 for i in `seq $count`; do
     echo "starting loop $i of $count"
-
-    oneCmd.py fps cobraMoveSteps phi stepsize=$steps
-    oneCmd.py mcs expose object exptime=$exptime doFibreID
+    
+    
+    oneCmd.py fps makeMotorMap phi stepsize=$steps totalsteps=1600 repeat=1 slowOnly
+    #oneCmd.py fps cobraMoveSteps phi stepsize=$steps
+    #oneCmd.py mcs expose object exptime=$exptime doFibreID
 done
--- bin/thetaCross.sh
+++ bin/thetaCross.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-steps=-30
+steps=-40
 count=40
 exptime=0.8
 
@@ -11,6 +11,7 @@ oneCmd.py mcs expose object exptime=$exptime doFibreID
 for i in `seq $count`; do
     echo "starting loop $i of $count"
 
-    oneCmd.py fps cobraMoveSteps theta stepsize=$steps
-    oneCmd.py mcs expose object exptime=$exptime doFibreID
+    oneCmd.py fps makeMotorMap theta stepsize=$steps totalsteps=1600 repeat=1 slowOnly
+    #oneCmd.py fps cobraMoveSteps theta stepsize=$steps
+    #oneCmd.py mcs expose object exptime=$exptime doFibreID
 done
