731de3a7e581836ba5346aaa20a6b4003482a256
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2022-05-19 14:37:37 -1000

    makeMotorMap should not in a loop

--- bin/phiCross.sh
+++ bin/phiCross.sh
@@ -8,11 +8,12 @@ oneCmd.py fps moveToHome all
 oneCmd.py iic moveToPfsDesign designId=0x4993e56d9549f2f9
 
 oneCmd.py mcs expose object exptime=$exptime doFibreID
-for i in `seq $count`; do
-    echo "starting loop $i of $count"
+
+oneCmd.py fps makeMotorMap phi stepsize=$steps totalsteps=1600 repeat=1 slowOnly
+
+#for i in `seq $count`; do
+#    echo "starting loop $i of $count"
     
-    
-    oneCmd.py fps makeMotorMap phi stepsize=$steps totalsteps=1600 repeat=1 slowOnly
     #oneCmd.py fps cobraMoveSteps phi stepsize=$steps
     #oneCmd.py mcs expose object exptime=$exptime doFibreID
-done
+#done
--- bin/thetaCross.sh
+++ bin/thetaCross.sh
@@ -8,10 +8,13 @@ oneCmd.py fps moveToHome all
 oneCmd.py iic moveToPfsDesign designId=0x24b4f9366954d539
 
 oneCmd.py mcs expose object exptime=$exptime doFibreID
-for i in `seq $count`; do
-    echo "starting loop $i of $count"
 
-    oneCmd.py fps makeMotorMap theta stepsize=$steps totalsteps=1600 repeat=1 slowOnly
+oneCmd.py fps makeMotorMap theta stepsize=$steps totalsteps=1600 repeat=1 slowOnly
+
+#for i in `seq $count`; do
+#    echo "starting loop $i of $count"
+
+#    oneCmd.py fps makeMotorMap theta stepsize=$steps totalsteps=1600 repeat=1 slowOnly
     #oneCmd.py fps cobraMoveSteps theta stepsize=$steps
     #oneCmd.py mcs expose object exptime=$exptime doFibreID
-done
+#done
