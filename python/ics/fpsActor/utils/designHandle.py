0e60f40ebb372b4e5c5fb9d94f4c0c97866bbf40
Arnaud Le Fur <afur@princeton.astro.edu>
2022-06-15 17:12:59 -1000

    designHandle can be provided maskFile only

--- python/ics/fpsActor/utils/designHandle.py
+++ python/ics/fpsActor/utils/designHandle.py
@@ -1,34 +1,31 @@
-from ics.fpsActor.utils import pfsDesign
-import pathlib
 import pandas as pd
+from ics.fpsActor.utils import pfsDesign
 
-class DesignFileHandle():
-    def __init__(self, designId, maskFile = None):
-     
-        self.maskFile = None
-        if maskFile is not None:
-            self.maskFile = maskFile
+
+class DesignFileHandle(object):
+    def __init__(self, designId, maskFile=None):
+
+        self.maskFile = maskFile
         self.designId = designId
         self.targets = None
         self.goodIdx = None
         self.badIdx = None
-        
-        self._loadTargets()
-        
+
+        if designId is not None:
+            self._loadTargets()
+
     def _loadTargets(self):
         targetPos = pfsDesign.loadPfsDesign(self.designId)
-        targets = targetPos[:,0]+targetPos[:,1]*1j
-        
+        targets = targetPos[:, 0] + targetPos[:, 1] * 1j
+
         self.targets = targets
- 
-    
+
     def loadMask(self):
-        
-        if self.maskFile is not None:  
+
+        if self.maskFile is not None:
             cobraInfo = pd.read_csv(self.maskFile)
             notMoveCobra = cobraInfo.loc[cobraInfo['bitMask'] == 0]
             doMoveCobra = cobraInfo.loc[cobraInfo['bitMask'] == 1]
 
-            self.badIdx = notMoveCobra['cobraId'].values-1
-            self.goodIdx = doMoveCobra['cobraId'].values-1
-
+            self.badIdx = notMoveCobra['cobraId'].values - 1
+            self.goodIdx = doMoveCobra['cobraId'].values - 1
